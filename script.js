1720aa8f80967a811d42673fbe08e5583c4b9b4c
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-24 10:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function firstWord(s) {
   // your code here
-	if(s.length){
-		return '';
-	}
-
-	const words = s.trim.split(' ');
-	return words[0];
+	  if (s.length === 0) {
+    return '';
+  }
+  
+  // Split the string by spaces
+  const words = s.trim().split(' ');
+  
+  // Return the first word
+  return words[0];
 }
 
 // Do not change the code below
